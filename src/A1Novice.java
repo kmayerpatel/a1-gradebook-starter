3211479ba5661f3955261b5c0c89d33ac8dd9c04
kmp <kmp@KMP.cs.unc.edu>
2018-07-05 15:33:09 -0400

    Added main processing loop.

--- src/A1Novice.java
+++ src/A1Novice.java
@@ -5,6 +5,12 @@ public class A1Novice {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
+		int student_count = scanner.nextInt();
 		
+		// Process each student in turn
+		
+		for (int i=0; i<student_count; i++) {
+			
+		}
 	}
 }
