fe2e347bfbb1aaad7d63a7fedc8565810d1cd797
Ketan Mayer-Patel <kmp@cs.unc.edu>
2018-08-24 15:30:09 -0400

    Add some basic comments to templates.

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -5,8 +5,12 @@ public class A1Adept {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
-		int student_count = scanner.nextInt();
+
+		// Your main program code here.
 		
 	}
+	
+	// Feel free to define addition methods here.
+	// Be sure to declare them as "public static"
+
 }
--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -5,8 +5,11 @@ public class A1Jedi {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
-		int student_count = scanner.nextInt();
+
+		// Your main program code here.
 		
 	}
+	
+	// Feel free to define addition methods here.
+	// Be sure to declare them as "public static"
 }
